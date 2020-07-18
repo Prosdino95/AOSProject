f95d8d42c13121d70372a5827274133983fcba34
Marco Prosdocimi <prosdino95@gmail.com>
2020-07-18 17:26:31 +0200

    Delete compile.sh

--- Test/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-clang-8 -O0 -Xclang -disable-O0-optnone -S -emit-llvm test.c -o out.ll 
-opt-8 -load../dist/usr/local/lib/BodyLoopExtractor.so -BodyLoopExtractor ./out.ll -S -o ./out2.ll
-#rm ./out.ll
-llc-8 ./out2.ll 
-#rm out2.ll
-../dist/user/local/bin/MOD-Llvm-mca out2.s
-
